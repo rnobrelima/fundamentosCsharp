0ef46dce5e66cf05524c4c71cfd484d2f36166f3
Rodrigo Nobre <rnobrelima@gmail.com>
2022-01-20 21:56:07 -0300

    StopWatch

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos Ex 10s");
+            Console.WriteLine("M = Minutos Ex 1m");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto Tempo Deseja Cronometrar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * multiplier);
+
+
+        }
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Finalizado");
+            Thread.Sleep(2000);
+            Menu();
+        }
+    }
+}
